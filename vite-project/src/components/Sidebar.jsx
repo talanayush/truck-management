895ae271d3ce70a732eec15b772af89855d8abfa
aryavsingla7 <116359284+Slemon30@users.noreply.github.com>
2024-08-16 20:55:24 +0530

    UI changes in Sidebar

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { HiMenu, HiX } from "react-icons/hi"; // You can use any icon library
+import { HiMenu, HiX, HiTruck, HiClipboardList, HiInbox } from "react-icons/hi"; // Importing icons
 
 const Sidebar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -19,34 +19,38 @@ const Sidebar = () => {
 
             {/* Sidebar */}
             <div
-                className={`fixed top-0 left-0 h-full bg-slate-800 text-white w-64 transform ${
+                className={`fixed top-0 left-0 h-full bg-slate-900 text-white w-64 transform ${
                     isOpen ? "translate-x-0" : "-translate-x-64"
                 } transition-transform duration-300 ease-in-out lg:translate-x-0 lg:relative lg:w-64`}
             >
-                <div className="p-4 pt-8"> {/* Added pt-8 for top padding */}
-                    <ul className="space-y-2">
+                <div className="p-6 pt-10"> {/* Added pt-10 for top padding */}
+                    
+                    <ul className="space-y-4">
                         <li>
                             <Link
                                 to="/send-delivery"
-                                className="block p-2 hover:bg-green-700 rounded"
+                                className="flex items-center p-3 hover:bg-green-700 rounded transition-colors duration-200"
                             >
-                                Send Truck
+                                <HiTruck className="mr-3" size={20} />
+                                <span>Send Truck</span>
                             </Link>
                         </li>
                         <li>
                             <Link
                                 to="/receive-delivery"
-                                className="block p-2 hover:bg-green-700 rounded"
+                                className="flex items-center p-3 hover:bg-green-700 rounded transition-colors duration-200"
                             >
-                                Receive Truck
+                                <HiInbox className="mr-3" size={20} />
+                                <span>Receive Truck</span>
                             </Link>
                         </li>
                         <li>
                             <Link
                                 to="/current-deliveries"
-                                className="block p-2 hover:bg-green-700 rounded"
+                                className="flex items-center p-3 hover:bg-green-700 rounded transition-colors duration-200"
                             >
-                                View All Deliveries
+                                <HiClipboardList className="mr-3" size={20} />
+                                <span>View All Deliveries</span>
                             </Link>
                         </li>
                     </ul>
